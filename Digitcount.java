7617abf5625c19154ae173d14dac83f35398daf9
lopamudradutta09876 <108073731+lopamudradutta09876@users.noreply.github.com>
2024-08-28 22:49:33 +0530

    Add files via upload

---
+++ Digitcount.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Digitcount {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("eneter a number");
+        int n=scan.nextInt();
+
+        int count=0;
+        while (n>0) {
+            n=n/10;
+            count++;
+            
+        }
+        System.out.println(count);
+    }
+    
+}
